75e5e2edefd4c0484dc3d3e2ca307376195b2ca1
daliborstakic <stakicdalibor64@gmail.com>
2020-09-02 11:34:32 +0200

    added class Node

--- astar.py
+++ astar.py
@@ -12,13 +12,45 @@ WIN = pygame.display.set_mode((WIDTH, WIDTH))
 # Colors
 WHITE = (255, 255, 255)
 
+class Node():
+    """ Class Node represents single cell in the grid """
+    def __init__(self, row, col, width, total_rows):
+        """ Init function """
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.width = width
+        self.neighbours = []
+        self.total_rows = total_rows
+
+    def is_color(self, color):
+        """ Checks the type of node by color """
+        return self.color == color
+
+    def set_color(self, color):
+        """ Sets the type of node by color """
+        self.color = color
+
+    def draw(self, win):
+        """ Drawing the individual nodes """
+        pygame.draw.rect(win, self.color, (self.x, self.y , self.width, self.width))
+
+    def update_neighbours(self):
+        pass
+
+    def __lt__(self, other):
+        """ Dunder method __lt__ if a tie happens """
+        return False
+
 def draw(win):
     """ Draws elements on the screen """
     win.fill(WHITE)
     pygame.display.update()
 
 def main(win):
-    """ Main method which handles all the function calls """
+    """ Main method which handles the function calls, also handles the input """
     run = True
 
     # Main loop
